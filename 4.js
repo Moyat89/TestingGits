dfef3be18360eee5c36ef2a4f86800bfd5f182af
Moyat89 <lagunamov@gmail.com>
2023-10-08 17:13:57 +0200

    C2

---
+++ 4.js
@@ -0,0 +1,16 @@
+
+/*
+Teo Alarcón Moya
+4.Crear un código que devuelva un array con los valores invertidos. Por ejemplo:
+- Dado el array [1,2,3,4,5] devolverá [5,4,3,2,1]
+- Dado [5,11,4,21,3] devolverá [3,21,4,11,5]*/
+
+(function () {
+    var array = [5,11,4,21,3];
+    var arrayOp=[];
+
+    for (var i=0;i<array.length;i++){
+        arrayOp[i]=array[(array.length-1)-i];
+    }
+    console.log(arrayOp);
+    })();
---
+++ 5.js
@@ -0,0 +1,19 @@
+/*Teo Alarcón Moya
+
+Queremos hacer un pequeño programa que nos calcule la letra del DNI.
+Dado el número del DNI, lo dividimos entre 23 y nos quedamos con el resto de dicha división.
+El resultado anterior es un número entre 0 y 22. A cada uno de estos posibles
+números le corresponde una letra, según la siguiente tabla:
+
+El programa deberá presentar el número del DNI que has introducido con su letra siguiendo el siguiente formato: 11111111-H
+Ejemplos de DNI:
+11111111-H
+27896454-F
+11247895-K*/
+
+(function () {
+    var numDNI =27896454 ; //Introduce número DNI
+    var arrayLetra=["T","R","W","A","G","M","Y","F","P","D","X","B","N","J","Z","S","Q","V","H","L","C","K","E"];
+    var posLetra= (numDNI%23);
+    console.log(numDNI+"-"+arrayLetra[posLetra]);
+    })();
